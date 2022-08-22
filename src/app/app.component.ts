b284bc3149a572a8267167fc166656b3c152a115
Luis <jorgedesarrollo717@gmail.com>
2022-08-22 04:34:56 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PIPES';
+
+  nombre:string="Jorge";
+  nombreCompleto:string='jorge rivera';
+  arreglo=[1,2,3,4,5,6,7,8,9];
+  pi=Math.PI;
+  a=0.234;
+  salario=1234.5;
+  heroe={
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad:500,
+    direccion:{
+      calle:"Primaria",
+      casa:"19"
+    }
+  }
+  valorPromesa = new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("Llego Data!"),3500);
+  });
+  fecha = new Date();
+  video="D9W4DLjmoOM";
+  activar=false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { registerLocaleData } from '@angular/common';
+// importar locales
+import localePy from '@angular/common/locales/es-PY';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+
+// registrar los locales con el nombre que quieras utilizar a la hora de proveer
+registerLocaleData(localePy, 'es');
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID,useValue:"es"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
